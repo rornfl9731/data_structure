5201a94a13a83f70ff3de2c24f22be3e69e34be3
Kim Jin Woo <inx0930@gmail.com>
2018-02-14 04:06:42 +0900

    stack problem

--- Balanced Parentheses Check.py
+++ Balanced Parentheses Check.py
@@ -22,17 +22,15 @@ def balance_check(s):
 
         if i=='}':
             if stack[-1]=='{':
-                print(stack[-1])
+
                 stack.pop()
 
         if i==']':
             if stack[-1]=='[':
-                print(stack[-1])
                 stack.pop()
 
         if i==')':
             if stack[-1]=='(':
-                print(stack[-1])
                 stack.pop()
 
     return stack==[]
@@ -41,5 +39,59 @@ print(balance_check('[](){([[[]]])}'))
 
 print(balance_check('()(){]}'))
 
+from nose.tools import assert_equal
 
+
+class TestBalanceCheck(object):
+    def test(self, sol):
+        assert_equal(sol('[](){([[[]]])}('), False)
+        assert_equal(sol('[{{{(())}}}]((()))'), True)
+        assert_equal(sol('[[[]])]'), False)
+        print('ALL TEST CASES PASSED')
+
+
+# Run Tests
+
+t = TestBalanceCheck()
+t.test(balance_check)
+
+'''
+모범답안
+def balance_check(s):
+    
+    # Check is even number of brackets
+    if len(s)%2 != 0:
+        return False
+    
+    # Set of opening brackets
+    opening = set('([{') 
+    
+    # Matching Pairs
+    matches = set([ ('(',')'), ('[',']'), ('{','}') ]) 
+    
+    # Use a list as a "Stack"
+    stack = []
+    
+    # Check every parenthesis in string
+    for paren in s:
+        
+        # If its an opening, append it to list
+        if paren in opening:
+            stack.append(paren)
+        
+        else:
+            
+            # Check that there are parentheses in Stack
+            if len(stack) == 0:
+                return False
+            
+            # Check the last open parenthesis
+            last_open = stack.pop()
+            
+            # Check if it has a closing match
+            if (last_open,paren) not in matches:
+                return False
+            
+    return len(stack) == 0
+'''
 
